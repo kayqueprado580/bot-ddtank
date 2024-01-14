591a01e4adbc819663249ad2843e54bd57bb7622
Kayque Prado Silva <46896994+kayqueprado580@users.noreply.github.com>
2024-01-14 17:42:56 -0300

    .ignore e setup

--- anthill/setup.py
+++ anthill/setup.py
@@ -1,20 +1,13 @@
 from cx_Freeze import setup, Executable
 
-# Lista de scripts que você deseja incluir no executável
 scripts = ["anthill.py"]
-
-packages = ["numpy", "PIL"],
-
-# Lista de módulos que você deseja incluir
+packages = (["numpy", "PIL"])
 includes = ["pyautogui", "time", "cv2", "logging", "keyboard", "ctypes"]
-
-# Lista de imagens que você deseja incluir
 include_files = [("img", "img"), ("log", "log"), ("pandora", "pandora")]
 
-# Configurações para o executável
 options = {
     "build_exe": {
-        "packages": packages
+        "packages": packages,
         "includes": includes,
         "include_files": include_files,
     }
